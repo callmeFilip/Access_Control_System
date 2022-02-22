66cb7c25cadefcfa7f20c1af4f013fe53113855a
callmeFilip <filip_andonow@abv.bg>
2022-02-22 16:58:53 +0200

    LED class commit. Documentation included.

--- Raspberry_Pi/LED.cpp
+++ Raspberry_Pi/LED.cpp
@@ -1,5 +1,13 @@
 #include "LED.hpp"
 
+/**
+ * @brief Construct a new LED::LED object
+ * Sets GPIO direction as output and sets LED to OFF
+ *
+ * @param gpio_number Number of gpio
+ * @param IO_manager IO_manager instance responsible for logging
+ * @param inverted Is LED connected inverted
+ */
 LED::LED(int gpio_number, IOManager &IO_manager, bool inverted)
     : GPIO(gpio_number, IO_manager), m_inverted(inverted)
 {
@@ -17,11 +25,21 @@ LED::LED(int gpio_number, IOManager &IO_manager, bool inverted)
     log("LED initialized");
 }
 
+/**
+ * @brief Destroy the LED::LED object
+ *
+ */
 LED::~LED()
 {
     log("LED unitilized");
 }
 
+/**
+ * @brief Turns LED OFF or ON
+ *
+ * @param value GPIO Value for LED
+ * @return int
+ */
 int LED::setLed(const GPIO_VALUE &value)
 {
     if (m_inverted)
