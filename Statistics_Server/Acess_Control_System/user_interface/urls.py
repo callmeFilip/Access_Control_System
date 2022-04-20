85b26896c39df55d6e38411f3bb41b9b73712153
callmeFilip <filip_andonow@abv.bg>
2022-04-20 23:55:08 +0300

    Added urls for home, new, detailed, update, remove

--- Statistics_Server/Acess_Control_System/user_interface/urls.py
+++ Statistics_Server/Acess_Control_System/user_interface/urls.py
@@ -3,7 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home-page'),
-    path('add-user/', views.add_user, name='add-user-page'),
-    path('details/', views.details, name='details-page')
+    path('', views.EmployeeListView.as_view(), name='home-page'),
+    path('employee/new/', views.EmployeeCreateView.as_view(), name='add-user-page'),
+    path('employee/<int:pk>/', views.EmployeeDetailView.as_view(),
+         name='employee-details-page'),
+    path('employee/<int:pk>/update/', views.EmployeeUpdateView.as_view(),
+         name='employee-update-page'),
+    path('employee/<int:pk>/remove/', views.EmployeeDeleteView.as_view(),
+         name='employee-remove-page')
 ]
